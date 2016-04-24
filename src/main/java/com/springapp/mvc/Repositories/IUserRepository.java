4d78ccdb9585374318ad9d4f1146ce9b0f20ab89
Stadzior <Stadzior@gmail.com>
2016-04-24 22:19:38 +0200

    Project structure fixes.

--- src/main/java/com/springapp/mvc/Repositories/IUserRepository.java
+++ src/main/java/com/springapp/mvc/Repositories/IUserRepository.java
@@ -1,6 +1,6 @@
 package com.springapp.mvc.Repositories;
 
-import com.springapp.mvc.Model.User;
+import com.springapp.mvc.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
